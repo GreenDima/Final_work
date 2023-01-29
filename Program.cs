591def4f2218275cbd566eebca2c5a4a44470805
Dmitry <greendima13@mail.ru>
2023-01-29 14:56:31 +0300

    Second method

--- Program.cs
+++ Program.cs
@@ -11,6 +11,16 @@
     return arr;
 }
 
+int NewSizeArray(string [] array)
+{
+    int size = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length < 4) size++;
+    }
+    return size;
+}
+
 Console.Write("Введите размер массива:");
 int size = Convert.ToInt32(Console.ReadLine());
 string [] array = Array(size);
