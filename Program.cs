97103e570d2b61c3abc68c23208cdc0d81d0fe46
Dmitry <greendima13@mail.ru>
2023-01-29 14:46:34 +0300

    First method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿string [] Array(int size)
+{
+    string [] arr = new string[size];
+    Console.WriteLine($"Введите {size} слов(а): ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{i + 1} слово: ");
+        arr[i] = Console.ReadLine();
+        Console.Clear();
+    }
+    return arr;
+}
+
+Console.Write("Введите размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+string [] array = Array(size);
+int newsize = NewSizeArray(array);
